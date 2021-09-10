f6bba7efc10f3511a13abde445cccdafa32d5ef9
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-09-10 14:55:19 +0200

    docs(desktop-client): update changelog

--- src/renderer/changelog.tsx
+++ src/renderer/changelog.tsx
@@ -3,11 +3,10 @@ import React from 'react';
 // place your changelog here
 const changelog = (
   <ul>
-    <li>write titlebar logic, fix modal visual glitch</li>
-    <li>add modals, fix sidebar transition, improve theming</li>
-    <li>make titlebar draggable</li>
-    <li>setup title bar</li>
-    <li>rewrite scss file structure to match boilerplate requirements</li>
+    <li>fix titlebar blur bug on modal open</li>
+    <li>fix regex disk filesystem filtering in dashboard</li>
+    <li>add spell checking to the server edit and add modals</li>
+    <li>minor ui tweaks</li>
   </ul>
 );
 
